2f0280b6e34013a73fb40278c919644ac1c674ce
cuhey3 <uotaneet@gmail.com>
2016-08-28 12:34:12 +0900

    first commit

--- src/main/java/com/heroku/myapp/hawtiosample/HatwioMain.java
+++ src/main/java/com/heroku/myapp/hawtiosample/HatwioMain.java
@@ -1,6 +1,7 @@
 package com.heroku.myapp.hawtiosample;
 
 import io.hawt.embedded.Main;
+import java.util.Optional;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -9,7 +10,8 @@ public class HatwioMain {
     public HatwioMain() throws Exception {
         Main main = new Main();
         System.setProperty("hawtio.authenticationEnabled", "false");
-        main.setPort(Integer.parseInt("2525"));
+        String port = Optional.ofNullable(System.getenv("PORT")).orElse("2525");
+        main.setPort(Integer.parseInt(port));
         main.setContextPath("/foo");
         main.setWarLocation("./");
         main.run();
